8e48a1bf970c90a2ff6e1fd3846cccc1ed1eaa04
tanmaysayare <tanmaysayare12345@gmail.com>
2024-10-11 23:23:15 +0530

    Done simple print

--- CPP/Demo.cpp
+++ CPP/Demo.cpp
@@ -1,9 +1,11 @@
+#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int x;
-    cin>>x;
-    cout<<"Hello " << x << endl;
+    int x , y;
+    cin >> y;
+    cin>> x ;
+    cout<<"Value of X : " << x << " , Y :" << y << endl;
     return 0;
 }
